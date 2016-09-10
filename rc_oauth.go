7483bc3760a0abb5819fc0e43bd3896a691c8253
John <jjkiely@gmail.com>
2016-09-10 17:07:33 -0400

    config now encapsulated in typestruct

--- rc_oauth.go
+++ rc_oauth.go
@@ -6,22 +6,35 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
-	//"strings"
 )
 
 ///////////////////////////////////////////////////////
 // Config:
 ///////////////////////////////////////////////////////
 
-var rcConfig = &oauth2.Config{
-	Scopes: []string{"public"},
-	Endpoint: oauth2.Endpoint{
-		AuthURL:  "https://recurse.com/oauth/authorize",
-		TokenURL: "https://recurse.com/oauth/token",
-	},
+type RCConfig struct {
+	*oauth2.Config
+	StateString string
 }
 
-var oauthStateString = getStateString(20)
+func MakeConfig(url, id, secret string) *RCConfig {
+	c := &oauth2.Config{
+		Scopes: []string{"public"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://recurse.com/oauth/authorize",
+			TokenURL: "https://recurse.com/oauth/token",
+		},
+		RedirectURL: url,
+		ClientID: id,
+		ClientSecret: secret,
+	}
+	rcConfig := &RCConfig{
+		c,
+		getStateString(20),
+	}
+	return rcConfig
+}
+
 
 // Generates a random 20 char string, as per the protocol
 func getStateString(n int) string {
@@ -33,78 +46,14 @@ func getStateString(n int) string {
 	return string(randString)
 }
 
-// Takes the applications details and generates the Config object
-func SetConfigVars(url, id, secret string) {
-	rcConfig.RedirectURL = url
-	rcConfig.ClientID = id
-	rcConfig.ClientSecret = secret
-}
-
 // Generates the URL on which use user can give consent for the app to use their RC data
-func GetUrl() string {
-	url := rcConfig.AuthCodeURL(oauthStateString)
+func (c *RCConfig) GetUrl() string {
+	url := c.AuthCodeURL(c.StateString)
 	return url
 }
 
-///////////////////////////////////////////////////////
-// Redirect:
-///////////////////////////////////////////////////////
-
-//var postAuthRedirect = "/";
-//var authObject = *RCAuth{};
-
-// A default function to handle the auth redirect and set the token
-//func HandleRedirect(w http.ResponseWriter, r *http.Request) {
-//	state := r.FormValue("state")
-//    if ! IsStateString(state) {
-//        log.Fatal("invalid oauth state")
-//        http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-//        return
-//	}
-//	code := r.FormValue("code")
-//	authObject.SetToken(code)
-//	url := postAuthRedirect
-//	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
-//}
-
-// Set the url you want the user redirected to after HandleRedirect
-// Default is "/"
-//func GenHandleRedirect (url string, auth *RCAuth) func(http.ResponseWriter, *http.Request) {
-//	postAuthRedirect = url
-//	authObject = auth
-//	return HandleRedirect
-//}
-
-//func HandlerGen(url string) func(string, func(http.ResponseWriter, *http.Request)) {
-//	postAuthRedirect = url
-//	pattern := getPattern(rcConfig.RedirectURL)
-//	return http.HandleFunc(pattern, HandleRedirect)
-//}
-//
-//func getPattern(url string) string {
-//	pattern := "/" + strings.SplitN(url, "/", 4)[3]
-//	return pattern
-//}
-
-// Checks if the state string passed back matches the one the user sent
-func IsStateString(state string) bool {
-	return state == oauthStateString
-}
-
-///////////////////////////////////////////////////////
-// Auth:
-///////////////////////////////////////////////////////
-
-type RCAuth struct {
-	*oauth2.Token
-	BaseUrl      string
-	RecurserPath string
-	BatchPath    string
-	TokenParam   string
-}
-
-func MakeAuth(code string) RCAuth {
-	token, err := rcConfig.Exchange(oauth2.NoContext, code)
+func (c *RCConfig) MakeAuth(code string) RCAuth {
+	token, err := c.Exchange(oauth2.NoContext, code)
 	if err != nil {
 		log.Fatal(token)
 	}
@@ -116,16 +65,17 @@ func MakeAuth(code string) RCAuth {
 	return t
 }
 
-func (t *RCAuth) SetToken(code string) {
-	token, err := rcConfig.Exchange(oauth2.NoContext, code)
-	if err != nil {
-		log.Fatal(token)
-	}
-	t.AccessToken = token.AccessToken
-	t.TokenType = token.TokenType
-	t.RefreshToken = token.RefreshToken
-	t.Expiry = token.Expiry
-	t.TokenParam = genAccessParam(token.AccessToken)
+
+///////////////////////////////////////////////////////
+// Auth:
+///////////////////////////////////////////////////////
+
+type RCAuth struct {
+	*oauth2.Token
+	BaseUrl      string
+	RecurserPath string
+	BatchPath    string
+	TokenParam   string
 }
 
 func genAccessParam(token string) string {
@@ -151,6 +101,11 @@ func makeRequest(url string) string {
 	}
 	bodyS := string(body)
 	return bodyS
+}
+
+// Checks if the state string passed back matches the one the user sent
+func (c *RCConfig) IsStateString(state string) bool {
+	return state == c.StateString
 }
 
 ///////////////////////////////////////////////////////
